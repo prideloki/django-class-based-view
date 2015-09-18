3000c5de33c0620b57f24e62d5e0e6e4a84d064e
Thanakom <Thanakom@Macintosh.local>
2015-09-18 14:24:14 +0700

    filter model according to user organization

--- admin.py
+++ admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from myapp.models import Role, Book, Organization
+import logging
 # Register your models here.
+
+logger = logging.getLogger(__name__)
+
+class RoleAdmin(admin.ModelAdmin):
+    exclude = ('permissions',)
+
+
+class BookAdmin(admin.ModelAdmin):
+
+    def get_queryset(self, request):
+        queryset = super(BookAdmin, self).get_queryset(request)
+
+        user = request.user
+
+        if user.is_superuser:
+            return queryset
+
+        organization = Organization.objects.get(user=user)
+        logger.debug('Something went wrong!')
+        return queryset.filter(organization=organization)
+        
+
+
+class OrganizationAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Organization, OrganizationAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Role, RoleAdmin)
--- models.py
+++ models.py
@@ -1,11 +1,55 @@
 from django.db import models
+from django.utils import timezone
+from django.core.urlresolvers import reverse
+from django.contrib.auth.models import Group, User
 import datetime
+
+# class Role(Group):
+# Source:
+# http://stackoverflow.com/a/2182418
+#     rank = models.IntegerField(default=1)
+#     description = models.TextField(default='')
+
+#     def __str__(self):
+#         return self.name
+# Source:
+# http://stackoverflow.com/a/2182418
+
+
+class CampaignUser(models.Model):
+
+    user = models.OneToOneField(User)
+    activation_key = models.CharField(max_length=40, blank=True)
+    key_expires = models.DateTimeField(default=datetime.date.today())
+    is_approved = models.BooleanField(default=False)
+    is_agreed = models.BooleanField(default=False)
+
+    class Meta:
+
+        db_table = 'campaign_user'
+
+
+class Organization(Group):
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'organization'
+
+
+class Role(Group):
+    rank = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.name
 
 
 class Employee(models.Model):
     name = models.CharField(max_length=50)
     age = models.IntegerField()
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
+    note = models.TextField(null=True)
 
     class Meta:
         verbose_name = "employee"
@@ -35,14 +79,24 @@ class Author(models.Model):
     name = models.CharField(max_length=200)
     email = models.EmailField()
     headshot = models.ImageField(upload_to='author_headshots')
-    last_accessed = models.DateTimeField()
+    last_accessed = models.DateTimeField(default=timezone.now())
 
     def __str__(self):              # __unicode__ on Python 2
         return self.name
+
+    def get_absolute_url(self):
+        return reverse('author-detail', kwargs={'pk': self.pk})
 
 
 class Book(models.Model):
     title = models.CharField(max_length=100)
     authors = models.ManyToManyField('Author')
+    organization = models.ForeignKey(Organization, null=True)
     publisher = models.ForeignKey(Publisher)
     publication_date = models.DateField()
+
+    class Meta:
+        default_permissions = ('add', 'change', 'delete', 'view')
+
+    def __str__(self):
+        return self.title
